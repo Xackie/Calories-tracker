e7fb477ce52e3fbf64fb354c26e6c7784e9f2d6e
Xackie <gyaan10497@gmail.com>
2022-04-23 15:28:24 +0530

    Server confg. initialization

---
+++ Backend/server.js
@@ -0,0 +1,42 @@
+const express=require("express");
+const app=express();
+const cors=require("cors");
+const path=require("path");
+const { default: mongoose } = require("mongoose");
+
+const bodyParser=require("body-parser");
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const port=process.env.PORT || 8888;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// sending Access-Control-Allow-Origin request to sort the cors problems
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+
+//Mongoose connection
+// const uri=process.env.ATLAS_URI;
+mongoose.connect("mongodb+srv://Gyan-admin:Johnny@cluster0.etwvq.mongodb.net/test?retryWrites=true&w=majority")
+const connection=mongoose.connection;
+connection.once('open',()=>{
+    console.log("mongodb connected");
+})
+
+
+const mealsRouter=require('./routes/meals');
+app.use('/meals',mealsRouter);
+ 
+app.get("/",(req,res) => res.send("Response from the GET request"));
+
+app.listen(port,()=>{
+    console.log("App is listening to port:",{port})
+});
+
+
+
