92c017379ac980f935c88d450885bfdbf7d61acc
Xackie <gyaan10497@gmail.com>
2022-04-28 14:44:12 +0530

    trial

--- Backend/server.js
+++ Backend/server.js
@@ -4,12 +4,13 @@ const cors=require("cors");
 const path=require("path");
 const { default: mongoose } = require("mongoose");
 require('dotenv').config()
-
-const bodyParser=require("body-parser");
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 const port=process.env.PORT || 8888;
 
+
+const bodyParser=require("body-parser");
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -28,7 +29,6 @@ mongoose.connect(ATLAS_URI, {
       useNewUrlParser: true
     })
 const connection=mongoose.connection;
-// console.log(ATLAS_URI);
 connection.once('open',()=>{
     console.log("mongodb connected");
 })
@@ -36,12 +36,8 @@ connection.once('open',()=>{
 const mealsRouter=require('./routes/meals');
 app.use('/meals',mealsRouter);
  
-// app.get("/",(req,res) => res.send("Response from the GET request"));
-app.use(express.static(path.join('/meals', 'build')));
-    
-    app.get('/', function (req, res) {
-      res.sendFile(path.join('/meals', 'build', 'index.html'));
-    });
+app.get("/",(req,res) => res.send(ATLAS_URI));
+
     
 app.listen(port,()=>{
     console.log("App is listening to port:",{port})
