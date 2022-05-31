228cbc42f755430661158f80c1393c0b3ef7c377
Xackie <gyaan10497@gmail.com>
2022-05-31 11:53:55 +0530

    Screenshots

--- src/components/appbar/AppBar.js
+++ src/components/appbar/AppBar.js
@@ -1,61 +1,10 @@
 
-import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import Button from '@material-ui/core/Button';
-// import MenuIcon from '@material-ui/icons/Menu';
-import Modal from "react-modal";
-// import ModalDialog from './ModalDialog';
+import React from 'react'
 
-const Appbar = () => {
-
-  const useStyles = makeStyles(theme => ({
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-  }));
-
-  const [open, setOpen] = useState(false);
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-  const classes = useStyles();
-
+const AppBar=()=> {
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          className={classes.menuButton}
-        >
-          {/* <MenuIcon /> */}
-        </IconButton>
-        <Typography variant="h6" className={classes.title}>
-          Calorie-Tracker App
-        </Typography>
-        <Button color="inherit" onClick={handleOpen}>
-          Signup
-        </Button>
-        <Button color="inherit" onClick={handleOpen}>
-          Login
-        </Button>
-      </Toolbar>
-      <Modal open={open} handleClose={handleClose} />
-    </AppBar>
-  );
-};
+    <div className='Navbar'>Calorie Tracker-<span>App</span></div>
+  )
+}
 
-export default Appbar;
+export default AppBar
